3e56296b76ec59e28036450c0ec24d34ab8b5fa9
Serijio <serijiomainsett@gmail.com>
2023-10-30 12:23:39 +0100

    ranking personajes femeninos xdnt

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
 	public static void main(String[] args) {
-	System.System.out.println("Wenas, soy el primer Main y no sé que van a hacer el resto xd");
+	System.out.println("Wenas, soy el primer Main y no sé que van a hacer el resto xd");
+
+	int num1 = 1;
+	int num2 = 2;
+	int num3 = 3;
+
+	String primer = "Esdeath";
+	String segun = "Juraku";
+	String tercer = "Erza";
+
+	System.out.println("El orden de personajes femeninos de preferencia es:\n"
+				+ num1 + ": " + primer
+				+ num2 + ": " + segun
+				+ num3 + ": " + tercer)
 	}
 }
