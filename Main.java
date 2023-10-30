40bc56d320e3d78e20c891aa6067860bb0b9fc0b
Serijio <serijiomainsett@gmail.com>
2023-10-30 12:16:56 +0100

    main añadido

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-	System.System.out.println("Wenas, soy el primer Main y no sé que van a hacer el resto xd");
+	System.out.println("Wenas, soy el primer Main y no sé que van a hacer el resto xd");
 	}
 }
