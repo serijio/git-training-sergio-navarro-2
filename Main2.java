8d41a470c88f3b29eb416c0e3592073121304640
Serijio <serijiomainsett@gmail.com>
2023-10-30 12:48:14 +0100

    ranking masculino añadido OwO

---
+++ Main2.java
@@ -0,0 +1,18 @@
+public class Main2 {
+	public static void main(String[] args) {
+	System.out.println("Ahora toca hacer el ranking de caballeros OwO");
+
+	int num1 = 1;
+	int num2 = 2;
+	int num3 = 3;
+
+	String primer = "Hisoka";
+	String segun = "Levi";
+	String tercer = "Nanami";
+
+	System.out.println("El orden de personajes masculinos de preferencia es:\n
+				+ num1 + ": " + primer + "\n"
+				+ num2 + ": " + segun + "\n"
+				+ num3 + ": " + tercer);
+	}
+}
